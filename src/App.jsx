f96c7ff8322019b062aae1d3363326bc676b448e
Tanay Kumar <tanay25000@gmail.com>
2025-03-29 10:35:50 +0530

    single-post

--- src/App.jsx
+++ src/App.jsx
@@ -38,31 +38,29 @@ function App() {
         <Router>
             <div className="app-container">
                 <Navbar />
-                <ErrorBoundary>
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/login" element={<Login />} />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
 
-                        {/* Blog routes */}
-                        <Route path="/blogs" element={<Blogs />} />
-                        <Route
-                            path="/blog/:slug"
-                            element={
-                                <PostErrorBoundary>
-                                    <Singlepost />
-                                </PostErrorBoundary>
-                            }
-                        />
+                    {/* Blog routes */}
+                    <Route path="/blogs" element={<Blogs />} />
+                    <Route
+                        path="/blog/:slug"
+                        element={
+                            <PostErrorBoundary>
+                                <Singlepost />
+                            </PostErrorBoundary>
+                        }
+                    />
 
-                        {/* Other routes */}
-                        <Route path="/authors" element={<Authors />} />
-                        <Route path="/about" element={<AboutUs />} />
+                    {/* Other routes */}
+                    <Route path="/authors" element={<Authors />} />
+                    <Route path="/about" element={<AboutUs />} />
 
-                        {/* Redirects */}
-                        <Route path="/blog" element={<Navigate to="/blogs" replace />} />
-                        <Route path="*" element={<Navigate to="/" replace />} />
-                    </Routes>
-                </ErrorBoundary>
+                    {/* Redirects */}
+                    <Route path="/blog" element={<Navigate to="/blogs" replace />} />
+                    <Route path="*" element={<Navigate to="/" replace />} />
+                </Routes>
             </div>
         </Router>
     );
